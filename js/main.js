df1be2982e2519bb2eea8430f13ed33ff780e13e
maureenosei <46576755+maureenosei@users.noreply.github.com>
2020-09-20 12:27:38 -0400

    Added js files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-//comment here
+//comment here testing
+$(document).ready(function () {
+  console.log("ready!");
+});
+
+$(document).ready(function () {
+  console.log("zebra tooltip working");
+  new $.Zebra_Tooltips($(".zebra_tooltips"), {
+    max_width: 300,
+    max_height: 200,
+    //position: "right",
+  });
+});
+
+$(".my-news-ticker").AcmeTicker({
+  controls: {
+    prev: $(".acme-news-ticker-prev"),
+    toggle: $(".acme-news-ticker-pause"),
+    next: $(".acme-news-ticker-next"),
+  },
+});
+
+$(".my-news-ticker").AcmeTicker({
+  type: "horizontal",
+});
